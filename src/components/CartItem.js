437a029048377055cc6895a628a33665097134d5
Ori Baram <obrm770@gmail.com>
2023-02-25 18:25:07 +0200

    refactor code

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -8,21 +8,17 @@ const CartItem = ({ id, img, title, price, amount }) => {
       <div>
         <h4>{title}</h4>
         <h4 className='item-price'>${price}</h4>
-        {/* remove Button */}
         <Button className='remove-btn' type='remove' id={id}>
           remove
         </Button>
       </div>
       <div>
-        {/* increase amount */}
         <Button className='amount-btn' type='inc' id={id}>
           <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 20 20'>
             <path d='M10.707 7.05L10 6.343 4.343 12l1.414 1.414L10 9.172l4.243 4.242L15.657 12z' />
           </svg>
         </Button>
-        {/* amount */}
         <p className='amount'>{amount}</p>
-        {/* decrease amount */}
         <Button className='amount-btn' type='dec' id={id}>
           <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 20 20'>
             <path d='M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z' />
